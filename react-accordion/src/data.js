5710a537686a0f57930d3860c369979dfdc37028
Valentyn Holovkevych <76655890+holovkevych@users.noreply.github.com>
2021-12-05 11:54:05 +0200

    created react accordion

---
+++ react-accordion/src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    id: 1,
+    title: 'What is your name?',
+    info:
+      'My name is Valentyn',
+  },
+  {
+    id: 2,
+    title: 'How old are you?',
+    info:
+      'I am 25',
+  },
+  {
+    id: 3,
+    title: 'Where you live?',
+    info:
+      'I live in Ukraine',
+  },
+  {
+    id: 4,
+    title: 'What is your date of birthday?',
+    info:
+      '14 September 1996',
+  },
+  {
+    id: 5,
+    title: 'Where do you want to go on vacation?',
+    info:
+      'Greece, Portugal, Cuba',
+  },
+]
+export default questions
